632723e3ad61282a0116e199703a0351f7eed689
shaoziyang <shaoziyang@126.com>
2020-12-09 09:55:00 +0800

    add MCP4017/4018/4019 Digital POT drive

---
+++ misc/MCP401x/mcp401x.py
@@ -0,0 +1,17 @@
+from micropython import const
+
+class MCP4018:
+
+    def __init__(self, i2c, addr = 47):
+        self.i2c = i2c
+        self.addr = addr
+        self.tb = bytearray(1)
+        self.rb = bytearray(1)
+
+    def write(self, dat):
+        self.tb[0] = dat
+        self.i2c.writeto(self.addr, self.tb)
+        
+    def read(self):
+        self.i2c.readfrom_into(self.addr, self.rb)
+        return self.rb[0]
