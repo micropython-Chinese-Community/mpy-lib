d110c5024c73cfecd6b445774c2e8f0c451a5332
shaoziyang <shaoziyang@126.com>
2021-10-23 22:18:34 +0800

    Fixed parameter reference error in test.py

--- misc/DS3231/test.py
+++ misc/DS3231/test.py
@@ -19,6 +19,6 @@ ds.Time([12,10,0])
 
 ds.DateTime([2018,3,12,1,22,10,0])
 
-ds.ALARM(12, 20, 10, ds.PER_DISABLE)
-ds.ALARM(12, 20, 10, ds.PER_DAY)
+ds.ALARM(12, 20, 10, DS3231.PER_DISABLE)
+ds.ALARM(12, 20, 10, DS3231.PER_DAY)
 ds.ClearALARM()
