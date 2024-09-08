51a5137bcf331a4ade5aff9bd2452b303996dbad
shaoziyang <shaoziyang@126.com>
2024-09-08 17:29:22 +0800

    update I2C_LCD1602 demo

--- lcd/I2C_LCD1602/test.py
+++ lcd/I2C_LCD1602/test.py
@@ -8,12 +8,12 @@
 
 '''
 from machine import I2C, Pin
-from mp_i2c_lcd1602 import LCD1602
+from i2c_lcd1602 import I2C_LCD1602
 from time import sleep_ms
 
 i2c = I2C(1, sda=Pin(9), scl=Pin(10))
 
-LCD = LCD1602(i2c)
+LCD = I2C_LCD1602(i2c)
 
 LCD.puts("I2C LCD1602")
 n = 0
